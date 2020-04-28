69eccaf9ae0735f8ec24a002bd881f785e28b227
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-04-28 23:47:46 +0200

    init proyect

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Running33!</Text>
     </View>
   );
 }
@@ -12,8 +12,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ff6347",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#fff",
   },
 });
