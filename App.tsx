5dbb19b09fc9854b3a90a2e186fc141f4df4282e
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-04-30 13:47:02 +0200

    add stack navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Home } from "./src/home";
+import { About } from "./src/about";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Running33!</Text>
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +16,34 @@ const styles = StyleSheet.create({
     color: "#fff",
   },
 });
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: "Home",
+            headerStyle: {
+              backgroundColor: "#f4511e",
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Stack.Screen
+          name="About"
+          component={About}
+          options={{
+            title: "About",
+            headerStyle: {
+              backgroundColor: "#f4511e",
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationStackOptions } from "react-navigation-stack";
+
+export const Home: React.FC<NavigationStackOptions> = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to About"
+        onPress={() => navigation.navigate("About")}
+      />
+    </View>
+  );
+};
